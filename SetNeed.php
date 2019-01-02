c5358ac9580ff4782ad5bc2a420ac18ae4e8942a
s22588561 <s22588561@gmail.com>
2019-01-02 08:55:32 +0800

    使需求到達資料庫

--- SetNeed.php
+++ SetNeed.php
@@ -33,127 +33,127 @@ function rand()
 </head>
 <body>
 <h1>50期需求</h1>
-<form action="" >
+<form action="addfifty.php" method="post" name="fifty" id="fifty">
 <table  class="settable">
 <tr>
 <th>1</th>
- <td  ><input type="number" name="1s"  style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
+ <td  ><input type="number" name="need[]"  style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
  <th>2</th>
- <td><input type="number"  name="2s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>3</th>
- <td><input type="number"  name="3s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
  <th>4</th>
- <td><input type="number" name="4s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>5</th>
- <td><input type="number" name="5s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
 
 
  <th>6</th>
- <td><input type="number" name="6s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>7</th>
- <td><input type="number" name="7s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>8</th>
- <td><input type="number" name="8s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>9</th>
- <td><input type="number" name="9s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>10</th>
- <td><input type="number" name="10s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  </tr>
  <tr>
  <th>11</th>
- <td><input type="number" name="11s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>12</th>
- <td><input type="number" name="12s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>13</th>
- <td><input type="number" name="13s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>14</th>
- <td><input type="number" name="14s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>15</th>
- <td><input type="number" name="15s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
 
 
  <th>16</th>
- <td><input type="number" name="16s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>17</th>
- <td><input type="number" name="17s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>18</th>
- <td><input type="number" name="18s "style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[] "style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>19</th>
- <td><input type="number" name="19s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>20</th>
- <td><input type="number" name="20s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  </tr>
  <tr>
  <th>21</th>
- <td><input type="number" name="21s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>22</th>
- <td><input type="number" name="22s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>23</th>
- <td><input type="number" name="23s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>24</th>
- <td><input type="number"  name="24s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>25</th>
- <td><input type="number" name="25s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
 
 
  <th>26</th>
- <td><input type="number" name="26s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>27</th>
- <td><input type="number" name="27s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
  <th>28</th>
- <td><input type="number" name="28s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>29</th>
- <td><input type="number" name="29s"  style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]"  style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>30</th>
- <td><input type="number"  name="30s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  </tr>
  <tr>
  <th>31</th>
- <td><input type="number"  name="31s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>32</th>
- <td><input type="number"  name="32s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>33</th>
- <td><input type="number"  name="33s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>34</th>
- <td><input type="number"  name="34s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>35</th>
- <td><input type="number"  name="35s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
 
 
  <th>36</th>
- <td><input type="number"  name="36s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>37</th>
- <td><input type="number"  name="37s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>38</th>
- <td><input type="number"  name="38s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>39</th>
- <td><input type="number"  name="39s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>40</th>
- <td><input type="number"  name="40s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  </tr>
  <tr>
  <th>41</th>
- <td><input type="number"  name="41s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>42</th>
- <td><input type="number"  name="42s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>43</th>
- <td><input type="number" name="43s"  style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]"  style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>44</th>
- <td><input type="number"  name="44s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
  <th>45</th>
- <td><input type="number"  name="45s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50"value="0" ></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50"value="0" ></td>
 
 
  <th>46</th>
- <td><input type="number"  name="46s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>47</th>
- <td><input type="number"  name="47s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>48</th>
- <td><input type="number"  name="48s"style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
+ <td><input type="number"  name="need[]"style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0"></td>
  <th>49</th>
- <td><input type="number" name="49s" style="padding: 20px 10px; line-height: 28px;width: 50px;"min="0" max="50" value="0"></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;"min="0" max="50" value="0"></td>
  <th>50</th>
- <td><input type="number" name="50s" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
+ <td><input type="number" name="need[]" style="padding: 20px 10px; line-height: 28px;width: 50px;" min="0" max="50" value="0" ></td>
  </tr>
   </table>
 
