9896a295892111710491e3184847e096fbb486f5
kuanyupan <44218019+kuanyupan@users.noreply.github.com>
2019-01-05 17:17:48 +0800

    可update排名(缺個人score)

--- score.php
+++ score.php
@@ -1,6 +1,7 @@
 <?php
 require_once("dbconfig.php");
 global $db;
+// $tid = $_GET['tid'];
 $sql = "select * from orderform where tid=1";
 $stmt = mysqli_prepare($db, $sql);
 mysqli_stmt_execute($stmt); //執行SQL
@@ -15,13 +16,27 @@ while ($rs = mysqli_fetch_assoc($result)) {
     
 }
 echo "$total";
+
+// update總成本
 $sql1 = "update team set totalscore=$total where tid=1";
 $stmt1 = mysqli_prepare($db, $sql1); //prepare sql statement
 mysqli_stmt_execute($stmt1);  //執行SQL
 
-$sql2 = "select totalscore from team  order by totalscore";
+// 排名
+$sql2 = "select * from team  order by totalscore";
 $stmt2 = mysqli_prepare($db, $sql2); //prepare sql statement
 mysqli_stmt_execute($stmt2);  //執行SQL
+$result2 = mysqli_stmt_get_result($stmt2);
+$rank = 1;
+while($rs2 = mysqli_fetch_assoc($result2)){
+    $tid = $rs2['tid'];   
+    $sql3 = "update team set rank=$rank where tid=$tid";
+    $stmt3 = mysqli_prepare($db, $sql3); //prepare sql statement
+    mysqli_stmt_execute($stmt3); 
+    $rank = $rank + 1;
+}
 
-$sql3 = "update team set rank=$rank";
+// 個人成績
+
+
 ?>
